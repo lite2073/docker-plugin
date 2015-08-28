29e7313c663a43d3f637f73eac97ad121bdbe66d
Te Li <lite2073@gmail.com>
2015-08-28 11:48:24 -0700

    remove copyright

--- docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/provisioning/DockerTaskQueueListener.java
+++ docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/provisioning/DockerTaskQueueListener.java
@@ -1,16 +1,3 @@
-/*
- * Copyright (c) 1998-2015 Citrix Online LLC
- * All Rights Reserved Worldwide.
- *
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO CITRIX ONLINE
- * AND CONSTITUTES A VALUABLE TRADE SECRET.  Any unauthorized use,
- * reproduction, modification, or disclosure of this program is
- * strictly prohibited.  Any use of this program by an authorized
- * licensee is strictly subject to the terms and conditions,
- * including confidentiality obligations, set forth in the applicable
- * License and Co-Branding Agreement between Citrix Online LLC and
- * the licensee.
- */
 package com.nirima.jenkins.plugins.docker.provisioning;
 
 import java.util.Map;
